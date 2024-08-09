f981c9578735c1ee733a43e2179ac8d77a485e47
Lev Merino <merinolev96@gmail.com>
2024-08-09 10:09:26 -0600

    Range function

--- src/app/canvas-xy/canvas-xy.page.ts
+++ src/app/canvas-xy/canvas-xy.page.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-canvas-xy',
   templateUrl: './canvas-xy.page.html',
   styleUrls: ['./canvas-xy.page.scss'],
 })
-export class CanvasXyPage implements OnInit {
+export class CanvasXyPage {
+  axis_y: number = 0;
+  axis_x: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ionViewWillEnter() {
+    console.log('WILL ENTER');
+    this.drawCanvas();
   }
 
+  //* Funcion para dibujar rectangulo dentro de canvas
+  drawCanvas() {
+    const canvas = document.getElementById(
+      'canvas-container'
+    ) as HTMLCanvasElement;
+    const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+    const hght = ctx.canvas.height;
+    const mv_y = (this.axis_y / 100) * (hght - 10);
+
+    const wdth = ctx.canvas.width;
+    const mv_x = (this.axis_x / 100) * (wdth - 60);
+
+    ctx.clearRect(0, 0, wdth, hght);
+    ctx.fillStyle = 'red';
+    ctx.fillRect(mv_x, mv_y, 60, 10);
+  }
 }
